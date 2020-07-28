ae92ca72b4086644cae97bc7b5a4b0c738935188
gutty <ymgcmsts60@gmail.com>
2020-07-28 12:43:43 +0900

    写真共有アプリ(8)

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -35,6 +35,11 @@ window.axios.interceptors.request.use(config => {
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.axios.interceptors.response.use(
+    response => response,
+    error => error.response || error
+)
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
